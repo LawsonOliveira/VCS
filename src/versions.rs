54e0262d61d5bfc73aea9042adb049dbf1276c29
lucasvitoriano25 <lucasvitoriano25@gmail.com>
2023-05-28 20:31:59 +0200

    version created not tested

--- src/versions.rs
+++ src/versions.rs
@@ -1,14 +1,19 @@
 pub mod version_fn{
-    use crate::structs::structs_mod::{BranchChangesLog,FileChangeLog};
+    const PATH: &str = "myvcs/";
+
+    use crate::structs::structs_mod::{BranchChangesLog,CommitFiles};
     use diffy::{apply, Patch};
     use std::any::Any;
     use std::fs::File;
     use std::io::{self, Write};
+    use std::fs;
+
+    use crate::structs::StructWriter;
 
     //verify if version desired exists
     fn verify_if_version_exists(version_to_verify:&str,branch :&BranchChangesLog) -> Result< usize, std::io::Error> {
-        for i in 0..branch.files_changelogs.len(){
-            if branch.files_changelogs[i].hash_changelog.contains(&version_to_verify.to_owned()){
+        for i in 0..branch.commits_files.len(){
+            if branch.commits_files[i].commit_hash == version_to_verify{
                 return Ok(i);
             }
         }
@@ -21,56 +26,33 @@ pub mod version_fn{
         Ok(())
     }
     
-    pub fn change_version(version_to_verify:&str,branch :&BranchChangesLog) -> Result< (), std::io::Error>{
-        let index = verify_if_version_exists(version_to_verify,branch).unwrap();
-        let files_changelogs : &FileChangeLog = &branch.files_changelogs[index];
+    pub fn change_version(version_to_verify:&str) -> Result< (), std::io::Error>{
 
-        //get the position of the version
-        let Some(position) = files_changelogs.hash_changelog.iter().position(|x| x == version_to_verify) else{
-            panic!("Can't find version: '{version_to_verify}'");
-        };
+        let branch :BranchChangesLog = StructWriter::read_struct_from_file(PATH).unwrap();
 
         //split the vector [version,last_hash] rest
-        let (left, right) = files_changelogs.hash_changelog.split_at(position);
+        let index = verify_if_version_exists(version_to_verify, &branch).unwrap();
+        let (left, right) = branch.commits_files.split_at(index + 1);
         
-        let path_original = format!("{},{}",files_changelogs.original_file_path,files_changelogs.original_file);
-        let path_diff = &files_changelogs.hash_files_path;
+        for commit_file in left{
 
-        //apply the first patch to a file
-        let original_file = std::fs::File::open(path_original).expect("Could not open file.");
-        let text_original: String = serde_yaml::from_reader(original_file).expect("Could not read values.");
+            let mut concatenated_file = "".to_string();
+            let mut path_current_file = "".to_string();
 
-        let path_file = std::fs::File::open(format!("{}{}",path_diff,left[0])).expect("Could not open file.");
-        let text_diff: String = serde_yaml::from_reader(path_file).expect("Could not read values.");
-        
-        let text_diff_owned = text_diff.to_owned();
+            for file_change_log in commit_file.files_changelogs{
+                
+                let diff_path = format!("{}{}",file_change_log.hash_files_path,file_change_log.hash_changelog);
+                let diff_content = fs::read_to_string(&diff_path).expect("could not read hash file");
+                let patch = Patch::from_str(&diff_content).unwrap();
 
-        let patch = Patch::from_str(&text_diff_owned).unwrap();
-        
-        let mut concatenated_patch: String = "".to_string();
-        concatenated_patch = (apply(&text_original.to_owned(), &patch).unwrap());
-
-        //patch all others hash to the file
-        for i in 1..left.len(){
-            let hash = &left[i];
-            let f = std::fs::File::open(format!("{}{}",path_diff,hash)).expect("Could not open file.");
-			let text_diff: String = serde_yaml::from_reader(f).expect("Could not read values.");
-
-            let text_diff_owned = text_diff.to_owned();
-            let patch = Patch::from_str(&text_diff_owned).unwrap();
-            concatenated_patch = (apply(&concatenated_patch.to_owned(), &patch).unwrap());
+                concatenated_file = (apply(&concatenated_file.to_owned(), &patch).unwrap());
+                path_current_file = format!("{}{}",file_change_log.original_file_path.clone(),file_change_log.original_file.clone());
+            }
+            
+            //save the version desired in the working file
+            overwrite_file_with_string(&path_current_file,&concatenated_file)?; 
         }
-        
-        //save the version desired in the working file
-        let path_file_to_save : String = files_changelogs.last_file.clone();
-        overwrite_file_with_string(&path_file_to_save,&concatenated_patch)?; 
         Ok(())       
     } 
     //change current file with the version desired
-
-
-
-
-
-
 }
